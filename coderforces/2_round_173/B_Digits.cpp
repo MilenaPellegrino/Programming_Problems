75323fa6e5aad911c1a46d39e9d164336c6ed463
MilenaPellegrino <milenapellegrinoch@gmail.com>
2025-03-13 06:08:58 -0300

    Exs b completed

--- coderforces/2_round_173/B_Digits.cpp
+++ coderforces/2_round_173/B_Digits.cpp
@@ -33,57 +33,27 @@ ll fact(ll n){
     return res;
 }
 
+ll con(ll n, ll d, ll mod){
+    ll res = 0;
+    fore(i, 0, n){
+        res = (res * 10 + d) % mod;
+    }
+    return res;
+}
+
 void solve(){
     ll n; cin>>n;
     ll d; cin>>d;
     vi res; 
 
-    if(n>7){
-        n=7;
-    }
+    if(n>7){n=7;}
     ll facts = fact(n);
-    vi odds = {1, 3, 5, 7, 9};
 
-    for(ll divs : odds){
-        if(divs==1){
-            res.pb(1);}
-        if (divs == 3 || divs == 9) {
-                // Para divisor 3 o 9, la suma de los dígitos es d * n!
-                long long sum = d * n_fact;
-                if (sum % divs == 0) {
-                    cout << divisor << " ";
-                }
-        } else if (divs == 5) {
-                // Para divisor 5, el número debe terminar en 0 o 5
-            if (d == 5) {
-                    cout << "5 ";
-                }
-        } else if (divisor == 7) {
-                // Para divisor 7, usamos la propiedad de que si n >= 3, el número es divisible por 7
-            if (n >= 3 || d == 7) {
-                    cout << "7 ";
-                }
-            }
+    vi odds = {1, 3, 5, 7, 9};
+    for(ll nums : odds){
+        if(con(facts, d, nums) == 0){
+            res.pb(nums);
         }
-    }
-    res.pb(1);  //  All numbers are divisible by 1
-
-    vi odds = {1, 3, 5, 7, 9};
-
-    if(d%3 == 0 || n>=3){  // Div by 3
-        res.pb(3);
-    }
-
-    if(d == 5){  // Div by 5
-        res.pb(5);
-    }
-
-    if(d == 7 || n >= 3){  // Div by 7
-        res.pb(7);
-    }
-
-    if(d%9 == 0 || n>=6){  // Div by 9
-        res.pb(9);
     }
     fore(i, 0, sz(res)){
         cout<<res[i]<<" ";
