97d5cf246c137e6f4d2ccae04d01769a23cdeab5
MilenaPellegrino <milenapellegrinoch@gmail.com>
2025-03-14 05:54:08 -0300

    Add template and Exs C

--- coderforces/2_round_173/C_Sums_on_Segments.cpp
+++ coderforces/2_round_173/C_Sums_on_Segments.cpp
@@ -25,8 +25,82 @@ typedef vector<char> vc;
 
 template<class T>ostream&operator<<(ostream&o,vector<T>const&v){o<<"[ ";for(auto const&x:v)o<<x<<" ";return o<<"]";}
 
+const ll NUM_MIN = -1e9 + 10;
+const ll NUM_MAX = 1e9 + 10;
+
+ll max_subarray_sum(vi &arr){
+    ll max_sum = NUM_MIN;
+    ll max_actual = 0;
+    for(ll num : arr){
+        max_actual += num;
+        max_sum = max(max_sum, max_actual);
+        if(max_actual < 0){
+            max_actual = 0;
+        }
+    }
+    return max_sum;
+}
+
+ll min_subarray_sum(vi &arr){
+    ll min_sum = NUM_MAX;
+    ll min_actual = 0;
+    for(ll num : arr){
+        min_actual += num;
+        min_sum = min(min_sum, min_actual);
+        if(min_actual > 0){
+            min_actual = 0;
+        }
+    }
+    return min_sum;
+}
+
 void solve(){
     ll n; cin>>n;
+    vi a(n); 
+    fore(i, 0, n)cin>>a[i];
+    set<ll> res;
+    ll x;
+    res.insert(0);
+    //vi res; 
+    vi sin_x; 
+    fore(i, 0, n){
+        if(a[i] != 1  && a[i] != -1){
+            x = a[i];
+        }else{
+            sin_x.pb(a[i]);
+        }
+    }
+    ll min_sx = min_subarray_sum(sin_x);
+    ll max_sx = max_subarray_sum(sin_x);
+
+    //db(min_sx);
+    //db(max_sx);
+
+    fore(i, min_sx, max_sx + 1){
+        res.insert(i);
+    }
+
+    ll min_cx = min_subarray_sum(a);
+    ll max_cx = max_subarray_sum(a);
+
+    //db(min_cx);
+    //db(max_cx);
+
+    if (min_cx < x){
+        min_cx = min(x, min_cx+x);
+    }
+    fore(i, min_cx, max_cx + 1){
+        res.insert(i);
+    }
+
+    vi res_v (all(res));
+    sort(all(res_v));
+
+    cout<<sz(res_v)<<"\n";
+    fore(i, 0, sz(res_v)){
+        cout<<res_v[i]<<" ";
+    }
+    cout<<"\n";
 }
 
 int main(){
