2efe40b1cc63f64387c1be546cf38d7284aa6c07
MilenaPellegrino <milenapellegrinoch@gmail.com>
2025-03-19 12:25:41 -0300

    Add problem b

---
+++ omegaUp/concursos/Google_GSoC_2025_exts/B_Is_It_A_Tree.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fore(i,a,b) for(ll i=(a);i<(b);i++)
+#define rof(i, a, b) for(ll i=(b);i>(a);i--)
+#define forn(e,c) for(const auto &e : (c))
+#define db(x) cout<<#x<< " = "<<(x)<<endl
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fst first
+#define snd second
+#define str string
+#define pri(x) cout << (x) << "\n"
+#define mset(a,v) memset((a),(v),sizeof(a))
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0); 
+using ll = long long;
+using pii = pair<int,int>;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef vector<char> vc;
+
+template<class T>ostream&operator<<(ostream&o,vector<T>const&v){o<<"[ ";for(auto const&x:v)o<<x<<" ";return o<<"]";}
+
+const ll MAXN = 10e9 + 10;
+
+ll arr[MAXN];
+
+void solve(){
+    //ll n; cin>>n;
+    vi edges; 
+    ll num; 
+    while(cin>>num){
+        edges.pb(num);
+    }
+
+    fore(i, 0, MAXN)arr[i]=-1;
+
+    fore(i, 0, sz(edges)){
+        ll b = edges[i+1];
+        arr[edges[i+1]] = max(arr[edges[i+1]], 0LL);
+        arr[edges[i+1]] += 1;
+    }
+
+    fore(i, 0, 10){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    FIN; 
+    int t = 1;
+    //int t; cin>>t; 
+    while(t--){
+        solve();
+    }
+    return 0;
+}
