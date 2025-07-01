76213faa28768cacc27a5a1e1732dd5ad1ebc580
MilenaPellegrino <milenapellegrino@mi.unc.edu.ar>
2025-07-01 13:55:41 -0300

    Agregados round 3 y round 4

--- coderforces/3_Round_1032/C_Those_Who_Are_With_Us.cpp
+++ coderforces/3_Round_1032/C_Those_Who_Are_With_Us.cpp
@@ -42,12 +42,17 @@ void solve(){
     ll n; cin>>n;
     ll m; cin>>m; 
     vvi a(n, vi(m));
+    vvi b(n, vi(m));
     fore(i, 0, n){
         fore(j, 0, m){
-            cin>>a[i][j];
+            ll aij;
+            cin>>aij; 
+            a[i][j] = aij;
+            b[i][j] = aij;
         }
     }
 
+    // PRIMERO FILA DESPUES COLUMNA: 
     ll filam = -1; 
     ll colm = -1;
     ll maxf = -1;
@@ -71,6 +76,22 @@ void solve(){
             filam = i;
         }
     }
+    // db(filam); 
+    // cout<<endl<<endl;
+    // buscamos la fila con mas elementos del elemento maximo: 
+    ll con_mas_max = -1;
+    ll cant = 0; 
+    fore(i, 0, n){
+        cant = 0;
+        fore(j, 0, m){
+            if(a[i][j] == maxf)cant++;
+        }
+        if(cant > con_mas_max){
+            con_mas_max = cant;
+            filam = i;
+        }
+    }
+    // db(filam);
     // cout<<endl<<endl;
 
     // restamos este valor maximo: 
@@ -97,6 +118,28 @@ void solve(){
         }
     }
 
+    // buscsamos la columna con mas elementos del maximo 
+    ll con_mas_max_col = -1;
+    ll cant2 = 0; 
+    fore(j, 0, m){
+        cant2 = 0;
+        fore(i, 0, n){
+            if(a[i][j] == maxc)cant2++;
+        }
+        if(cant2 > con_mas_max_col){
+            con_mas_max_col = cant2;
+            colm = j;
+        }
+    }
+
+    // 3 2
+
+    // 0 1
+    // 1 0
+    // 1 1
+
+    // db(colm);
+    // restamos el elemento maximo a lla columna
     fore(i, 0, n){
         a[i][colm] = a[i][colm] - 1;
     }
@@ -107,8 +150,87 @@ void solve(){
             maxv = max(maxv, a[i][j]);
         }
     }
-    cout<<maxv<<"\n";
+    // cout<<maxv<<"\n";
 
+    // PRIMERO COLUMNA DESPUES FILA: 
+    // PRIMERO FILA DESPUES COLUMNA: 
+    ll filam5 = -1; 
+    ll colm5 = -1;
+    ll maxf5 = -1;
+    ll maxc5 = -1; 
+ // buscamos el maximo en las columnas: 
+    fore(j, 0, m){
+        ll maxca5 = -1;
+        fore(i, 0, n){
+            maxca5 = max(maxca5, b[i][j]);
+        }
+        if(maxca5 > maxc5){
+            maxc5 = maxca5;
+            colm5 = j;
+        }
+    }
+
+    // buscsamos la columna con mas elementos del maximo 
+    ll con_mas_max_col5 = -1;
+    ll cant5 = 0; 
+    fore(j, 0, m){
+        cant5 = 0;
+        fore(i, 0, n){
+            if(b[i][j] == maxc5)cant5++;
+        }
+        if(cant5 > con_mas_max_col5){
+            con_mas_max_col5 = cant5;
+            colm5 = j;
+        }
+    }  
+
+    fore(i, 0, n){
+        b[i][colm5] = b[i][colm5] - 1;
+    }
+
+    // PROCESO DE LA FILAS: 
+// buscamos el maximo en las filas: 
+    fore(i, 0, n){
+        ll maxfa6 = -1;
+        fore(j, 0, m){
+            maxfa6 = max(maxfa6, b[i][j]);
+        }
+        if(maxfa6 > maxf5){
+            maxf5 = maxfa6;
+            filam5 = i;
+        }
+    }
+    // db(filam); 
+    // cout<<endl<<endl;
+    // buscamos la fila con mas elementos del elemento maximo: 
+    ll con_mas_max6 = -1;
+    ll cant6 = 0; 
+    fore(i, 0, n){
+        cant6 = 0;
+        fore(j, 0, m){
+            if(b[i][j] == maxf5)cant6++;
+        }
+        if(cant6 > con_mas_max6){
+            con_mas_max6 = cant6;
+            filam5 = i;
+        }
+    }
+    // db(filam);
+    // cout<<endl<<endl;
+
+    // restamos este valor maximo: 
+    fore(j, 0, m){
+        b[filam5][j] = b[filam5][j] - 1;
+    }
+    
+    b[filam5][colm5] = b[filam5][colm5] + 1;
+    ll maxv2 = 0;
+    fore(i, 0, n){
+        fore(j, 0, m){
+            maxv2 = max(maxv2, b[i][j]);
+        }
+    }
+    cout<<min(maxv, maxv2)<<"\n";
 }
 
 int main(){
