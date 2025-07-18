a7fcc0a854db9bc19595d7c1b5cc343233a7e743
MilenaPellegrino <milenapellegrino@mi.unc.edu.ar>
2025-07-18 19:34:19 -0300

    Agregado varios problemas de divs y tca

---
+++ coderforces/unsorted/A_Creating_Words.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fore(i,a,b) for(ll i=(a);i<(b);i++)
+#define rof(i, a, b) for(ll i=(b);i>(a);i--)
+#define forn(e,c) for(const auto &e : (c))
+#define db(x) cout<<#x<< " = "<<(x)<<endl
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fst first
+#define snd second
+#define str string
+#define pri(x) cout << (x) << "\n"
+#define mset(a,v) memset((a),(v),sizeof(a))
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0); 
+using ll = long long;
+using pii = pair<int,int>;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef vector<char> vc;
+
+template<class T>ostream&operator<<(ostream&o,vector<T>const&v){o<<"[ ";for(auto const&x:v)o<<x<<" ";return o<<"]";}
+
+set<char> letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+set<char> numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+set<ll> nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+void solve(){
+    string a, b; cin>>a>>b; 
+    // string res1; 
+    // res1 += b[0]; 
+    // res1 += a[1]; 
+    // res1 += a[2]; 
+
+    // string res2; 
+    // res2 += a[0]; 
+    // res2 += b[1]; 
+    // res2 += b[2]; 
+
+    // cout<<res1<<" "<<res2<<"\n";
+    swap(a[0], b[0]); 
+    cout<<a<<" "<<b<<"\n";
+}
+
+int main(){
+    FIN; 
+    //int t = 1;
+    int t; cin>>t; 
+    while(t--){
+        solve();
+    }
+    return 0;
+}
