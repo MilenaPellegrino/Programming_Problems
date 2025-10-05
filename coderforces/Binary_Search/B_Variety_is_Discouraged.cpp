e1947154711b1083a494c7b380200ab5da2bcb54
MilenaPellegrino <milenapellegrino@mi.unc.edu.ar>
2025-10-04 22:24:50 -0300

    Agregado problemas varios

---
+++ coderforces/Binary_Search/B_Variety_is_Discouraged.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fore(i,a,b) for(ll i=(a);i<(b);i++)
+#define rof(i, a, b) for(ll i=(b);i>(a);i--)
+#define forn(e,c) for(const auto &e : (c))
+#define db(x) cout<<#x<< " = "<<(x)<<endl
+#define RAYA cout<<"=============="<<"\n"
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fst first
+#define snd second
+#define str string
+#define pri(x) cout << (x) << "\n"
+#define mset(a,v) memset((a),(v),sizeof(a))
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0); 
+using ll = long long;
+using pii = pair<int,int>;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef vector<char> vc;
+
+template<class T>ostream&operator<<(ostream&o,vector<T>const&v){o<<"[ ";for(auto const&x:v)o<<x<<" ";return o<<"]";}
+
+set<char> letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+set<char> numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+set<ll> nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+string YES = "YES\n"; 
+string NO = "NO\n";
+
+void solve(){
+    ll n; cin>>n;
+    vi a(n); fore(i, 0, n)cin>>a[i];
+    // bool ig = true; 
+    // fore(i, 0, n-1){
+    //     if(a[i] != a[i+1]){
+    //         ig = false; 
+    //         break;
+    //     }
+    // }
+    // if(ig){
+    //     pri(0); 
+    //     return;
+    // }
+    map<ll, ll> mp; 
+    // Num -> ocurrencias 
+    fore(i, 0, n){
+        ll ai = a[i]; 
+        mp[ai]++;
+    }
+    ll l = 0, r = n-1;
+    ll contmax = 0;
+    ll conttemp = 0;
+    ll ltem = 0;
+    ll  rtem = 0;
+    fore(i, 0, n){
+        if(mp[a[i]] == 1){
+            conttemp++;
+            rtem = i;
+        }else{
+            if(conttemp > contmax){
+                //RAYA;
+                //db(i); db(a[i]);
+                contmax = conttemp; 
+                l = ltem; 
+                r = rtem;
+            }
+            conttemp = 0;
+            ltem = i+1;
+        }
+    }
+
+    if(conttemp > contmax){
+        contmax = conttemp; 
+        l = ltem;
+        r = rtem;
+    }
+    if(contmax == 0){
+        pri(0);
+        return;
+    }
+    cout<<l+1<<" "<<r+1<<"\n";
+}
+
+int main(){
+    FIN; 
+    //int t = 1;
+    int t; cin>>t; 
+    while(t--){
+        solve();
+    }
+    return 0;
+}
