a9648745d75209367e4943a17ead2c2ac715e6dc
Milena Pellegrino <milenapellegrino@mi.unc.edu.ar>
2026-03-25 21:39:39 -0300

    Add problem d

--- Training Camp and Courses/TCArg 2025/Inicial/1/d.cpp
+++ Training Camp and Courses/TCArg 2025/Inicial/1/d.cpp
@@ -15,8 +15,72 @@ typedef vector<ll> vi;
 int main(){
     FIN;
     ll t; cin>>t; 
-    ll n; cin>>n; 
-    vi a(n); fore(i, 0, n)cin>>a[i];
+    while(t--){
+        ll n; cin>>n; 
+        vi a(n); fore(i, 0, n)cin>>a[i];
+        vi freq(n+1, 0);
+        vector<vector<ll>> defreq(n+1); 
+        fore(i, 0, n){
+            freq[a[i]]++;
+        }
+
+        ll mex = -1;
+        fore(i, 0, sz(freq)){
+            if(freq[i] == 0){mex = i; break;}
+        }
+        if(mex == -1)mex = n+1;
+        //cout<<mex<<endl;
+        fore(i, 0, mex+1){
+            defreq[freq[i]].pb(i);
+        }
+        vi res;
+        set<ll> mexpos; 
+
+        // k=0 -> puedo mex = i?
+        fore(i, 0, mex+1){
+            if(i<= (n) && freq[i]<= 0){
+                mexpos.insert(i);
+            }
+        }
+        res.pb(sz(mexpos));
+        // for (ll elem : mexpos){
+        //     cout<<elem<<" ";
+        // }
+        // cout<<endl;
+        // fore(i, 0, sz(res)){
+        //     cout<<res[i]<<" ";
+        // }
+        // cout<<endl;
+        // mexpos.clear();
+        // // k=1 -> puedo mex = i? 
+        // fore(i, 0, n-1){
+        //     if(i<= (n-1) && freq[i]<= 1){
+        //         mexpos.insert(i);
+        //     }
+        // }
+        //res.pb(sz(mexpos)); 
+
+        fore(i, 1, n+1){
+            //mexpos.clear();
+            ll sacar =  n - i + 1;
+            mexpos.erase(sacar);
+            for(ll k : defreq[i]){
+                if(k <= mex && k <= n-i){
+                    mexpos.insert(k);
+                }
+            }
+            res.pb(sz(mexpos));
+            //res.pb(sz(mexpos));
+        }
+
+
+        fore(i, 0, sz(res)){
+            cout<<res[i]<<" ";
+        }
+        cout<<"\n";
+
+    }
+
     
     return 0;
 }
